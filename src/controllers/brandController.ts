6ddcf1e125f18520db059ac333fa0e9d5f5640c5
Ivan Epou <ivanepou92@gmail.com>
2024-09-22 23:41:03 +0300

    Add brandService and modify the brandController

--- src/controllers/brandController.ts
+++ src/controllers/brandController.ts
@@ -1,79 +1,39 @@
 import { Request, Response } from "express";
 import { db } from "@/db/db";
+import { brandService } from "@/services/brandService";
 
 export const createBrand = async (req: Request, res: Response) => {
-  const { name, slug, image } = req.body;
-  const brandExists = await db.brand.findUnique({
-    where: {
-      slug,
-    },
-  });
-  if (brandExists) {
-    return res.status(409).json({
-      error: `Brand with slug: ${slug} already exists`,
-    });
-  }
+  //call the createBrand service
   try {
-    const newBrand = await db.brand.create({
-      data: {
-        name,
-        slug,
-        image,
-      },
-    });
-    return res.status(201).json({
-      data: newBrand,
-      error: null,
-      message: "Brand created successfully",
-    });
-  } catch (error) {
-    console.error("Error creating Brand:", error);
-    return res.status(500).json({
-      error: "An unexpected error occurred. Please try again later.",
-    });
+    const brand = await brandService.createBrand(req.body);
+    return res.status(201).json(brand);
+  } catch (error: any) {
+    return res
+      .status(500)
+      .json({ error: `Failed to create brand ${error.message}` });
   }
 };
 
 export const getBrands = async (_req: Request, res: Response) => {
   try {
-    const brands = await db.brand.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    return res.status(200).json({
-      data: brands,
-    });
-  } catch (err: any) {
-    console.log(err);
-    return res.status(201).json({
-      error: `An unexpected error occurred. Please try again later.`,
-    });
+    const brands = await brandService.getBrands();
+    return res.status(200).json(brands);
+  } catch (error: any) {
+    return res
+      .status(500)
+      .json({ error: `Failed to get brands ${error.message}` });
   }
 };
 
 export const getBrand = async (req: Request, res: Response) => {
   const id = req.params.id;
   try {
-    const brand = await db.brand.findUnique({
-      where: {
-        id,
-      },
-    });
-    if (!brand) {
-      return res.status(404).json({
-        data: null,
-        error: `Brand not found.`,
-      });
-    }
-    return res.status(200).json({
-      data: brand,
-    });
+    const brand = await brandService.getBrand(id);
+    return res.status(200).json(brand);
   } catch (error: any) {
-    console.log(error);
-    return res.status(201).json({
-      error: `An unexpected error occurred. Please try again later.`,
-    });
+    return res
+      .status(500)
+      .json({ error: `Failed to get brand ${error.message}` });
   }
 };
 
--- src/services/brandService.ts
+++ src/services/brandService.ts
@@ -1,1 +1,108 @@
 import { db } from "@/db/db";
+import { Prisma } from "@prisma/client";
+//create a service to create a brand
+export const createBrand = async (brand: Prisma.BrandCreateInput) => {
+  const { name, slug, image } = brand;
+  const brandExists = await db.brand.findUnique({
+    where: {
+      slug,
+    },
+  });
+  if (brandExists) {
+    throw new Error(`Brand with slug: ${slug} already exists`);
+  }
+  try {
+    const newBrand = await db.brand.create({
+      data: {
+        name,
+        slug,
+        image,
+      },
+    });
+    return {
+      data: newBrand,
+      error: null,
+      message: "Brand created successfully",
+    };
+  } catch (error) {
+    console.error("Error creating Brand:", error);
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new Error(error.message);
+    } else
+      throw new Error("An unexpected error occurred. Please try again later.");
+  }
+};
+
+//Get all brands
+export const getBrands = async () => {
+  try {
+    const brands = await db.brand.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return {
+      data: brands,
+    };
+  } catch (err: any) {
+    console.log(err);
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new Error(err.message);
+    } else {
+      throw new Error("An unexpected error occurred. Please try again later.");
+    }
+  }
+};
+
+//Get a brand by id
+export const getBrand = async (id: string) => {
+  try {
+    const brand = await db.brand.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!brand) {
+      throw new Error("Brand not found.");
+    }
+
+    return {
+      data: brand,
+    };
+  } catch (error: any) {
+    console.log(error);
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("An unexpected error occurred. Please try again later.");
+    }
+  }
+};
+
+// //create a service to update a brand
+// export const updateBrand = async (id: string, brand: any) => {
+//   try {
+//     const updatedBrand = await db.collection("brands").doc(id).update(brand);
+//     return updatedBrand;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// //create a service to delete a brand
+// export const deleteBrand = async (id: string) => {
+//   try {
+//     const deletedBrand = await db.collection("brands").doc(id).delete();
+//     return deletedBrand;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+export const brandService = {
+  createBrand,
+  getBrands,
+  getBrand,
+  // updateBrand,
+  // deleteBrand,
+};
