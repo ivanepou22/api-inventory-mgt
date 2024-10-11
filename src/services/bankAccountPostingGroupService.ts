af51aaaa53ccee2a2ce29e0c4122aff3bf380326
Ivan Epou <ivanepou92@gmail.com>
2024-10-11 23:59:57 +0300

    Fix index keys errors

---
+++ src/services/bankAccountPostingGroupService.ts
@@ -0,0 +1,7 @@
+import { db } from "@/db/db";
+import { Prisma } from "@prisma/client";
+
+//create a service to create a bank account posting group
+export const createBankAccountPostingGroup = async (
+  bankAccountPostingGroup: Prisma.BankAccountPostingGroupCreateInput
+) => {};
