4e9859f279b92b8a6ca494e2533152f8d379e531
Ivan Epou <ivanepou92@gmail.com>
2024-10-24 23:27:50 +0300

    Update noSeriesLineService with multitenancy

--- src/services/noSeriesLineService.ts
+++ src/services/noSeriesLineService.ts
@@ -1,223 +1,336 @@
 import { db } from "@/db/db";
-import { Prisma } from "@prisma/client";
+import { slugify } from "@/utils/functions";
+import { MultiTenantService } from "@/services/multiTenantService";
+import { Prisma, PrismaClient } from "@prisma/client";
 
-const createNoSeriesLine = async (
-  noSeriesLine: Prisma.NoSeriesLineUncheckedCreateInput
-) => {
-  const {
-    noSeriesId,
-    startingDate,
-    endingDate,
-    startingNo,
-    endingNo,
-    lastDateUsed,
-    lastNoUsed,
-    lastDigitUsed,
-    increment,
-  } = noSeriesLine;
-  try {
-    //get the noSeries
-    const noSeries = await db.noSeries.findUnique({
-      where: { id: noSeriesId },
-    });
-    if (!noSeries) {
-      throw new Error("No Series not found");
-    }
-    const noSeriesCode = noSeries.code;
-
-    //the start date should always be less than the ending date and the ending date should be greater than the starting date
-    if (startingDate && endingDate) {
-      if (startingDate > endingDate) {
-        throw new Error(
-          "The starting date should be less than the ending date."
-        );
-      }
-      if (endingDate < startingDate) {
-        throw new Error(
-          "The ending date should be greater than the starting date."
-        );
-      }
-    }
-    const noSeriesLine = await db.noSeriesLine.create({
-      data: {
-        noSeriesId,
-        noSeriesCode,
-        startingDate,
-        endingDate,
-        startingNo,
-        endingNo,
-        lastDateUsed,
-        lastNoUsed,
-        lastDigitUsed,
-        increment,
-      },
-    });
-
-    return {
-      data: noSeriesLine,
-      message: "No Series Line created successfully",
-    };
-  } catch (error: any) {
-    console.error("Error creating No Series Line:", error);
-    throw new Error("Failed to create No Series Line");
+export class NoSeriesLineService extends MultiTenantService {
+  constructor(db: PrismaClient) {
+    super(db);
   }
-};
-
-const getNoSeriesLines = async () => {
-  try {
-    const noSeriesLines = await db.noSeriesLine.findMany();
-    return {
-      data: noSeriesLines,
-      message: "No Series Lines fetched successfully",
-    };
-  } catch (error: any) {
-    console.error("Error fetching No Series Lines:", error);
-    throw new Error("Failed to fetch No Series Lines");
-  }
-};
-
-const getNoSeriesLine = async (id: string) => {
-  try {
-    const noSeriesLine = await db.noSeriesLine.findUnique({
-      where: { id },
-    });
-
-    if (!noSeriesLine) {
-      throw new Error("No Series Line not found");
-    }
-
-    return {
-      data: noSeriesLine,
-      message: "No Series Line fetched successfully",
-    };
-  } catch (error: any) {
-    console.error("Error fetching No Series Line:", error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+  createNoSeriesLine = async (
+    noSeriesLine: Prisma.NoSeriesLineUncheckedCreateInput
+  ) => {
+    const {
+      noSeriesId,
+      startingDate,
+      endingDate,
+      startingNo,
+      endingNo,
+      lastDateUsed,
+      lastNoUsed,
+      lastDigitUsed,
+      increment,
+    } = noSeriesLine;
+    try {
+      //get the noSeries
+      const noSeries = await this.findUnique(
+        (args) => this.db.noSeries.findUnique(args),
+        {
+          where: { id: noSeriesId },
+        }
       );
-    } else {
-      throw new Error(error.message);
-    }
-  }
-};
-
-const updateNoSeriesLine = async (
-  id: string,
-  noSeriesLine: Prisma.NoSeriesLineUncheckedCreateInput
-) => {
-  const {
-    startingDate,
-    endingDate,
-    startingNo,
-    endingNo,
-    lastDateUsed,
-    lastNoUsed,
-    lastDigitUsed,
-    increment,
-  } = noSeriesLine;
-  try {
-    //check if the noSeriesLine exists
-    const noSeriesLineExists = await db.noSeriesLine.findUnique({
-      where: { id },
-    });
-    if (!noSeriesLineExists) {
-      throw new Error("No Series Line not found");
-    }
-
-    if (startingDate && endingDate) {
-      if (startingDate > endingDate) {
-        throw new Error(
-          "The starting date should be less than the ending date."
-        );
+      if (!noSeries) {
+        throw new Error("No Series not found");
       }
-      if (endingDate < startingDate) {
-        throw new Error(
-          "The ending date should be greater than the starting date."
-        );
+      const noSeriesCode = noSeries.code;
+
+      const noSeriesLineExists = await this.findUnique(
+        (args) => this.db.noSeriesLine.findUnique(args),
+        {
+          where: {
+            tenantId_companyId_noSeriesId_startingNo: {
+              tenantId: this.getTenantId(),
+              companyId: this.getCompanyId(),
+              noSeriesId,
+              startingNo: await slugify(startingNo),
+            },
+          },
+        }
+      );
+      if (noSeriesLineExists) {
+        throw new Error("No Series Line already exists");
       }
-    } else if (startingDate) {
-      if (noSeriesLineExists.endingDate) {
-        if (startingDate > noSeriesLineExists.endingDate) {
+
+      //the start date should always be less than the ending date and the ending date should be greater than the starting date
+      if (startingDate && endingDate) {
+        if (startingDate > endingDate) {
           throw new Error(
             "The starting date should be less than the ending date."
           );
         }
-        if (noSeriesLineExists.endingDate < startingDate) {
+        if (endingDate < startingDate) {
           throw new Error(
             "The ending date should be greater than the starting date."
           );
         }
       }
-    } else if (endingDate && noSeriesLineExists.startingDate) {
-      if (endingDate < noSeriesLineExists.startingDate) {
+      const noSeriesLine = await this.create(
+        (args) => this.db.noSeriesLine.create(args),
+        {
+          data: {
+            noSeriesId,
+            noSeriesCode,
+            startingDate,
+            endingDate,
+            startingNo: await slugify(startingNo),
+            endingNo,
+            lastDateUsed,
+            lastNoUsed,
+            lastDigitUsed,
+            increment,
+            tenantId: this.getTenantId(),
+            companyId: this.getCompanyId(),
+          },
+        }
+      );
+
+      return {
+        data: noSeriesLine,
+        message: "No Series Line created successfully",
+      };
+    } catch (error: any) {
+      console.error("Error creating No Series Line:", error);
+      throw new Error("Failed to create No Series Line");
+    }
+  };
+
+  getNoSeriesLines = async () => {
+    try {
+      const noSeriesLines = await this.findMany(
+        (args) => this.db.noSeriesLine.findMany(args),
+        {
+          orderBy: {
+            createdAt: "desc",
+          },
+          include: {
+            noSeries: {
+              select: {
+                id: true,
+                code: true,
+                name: true,
+              },
+            },
+            company: {
+              select: {
+                id: true,
+                code: true,
+                name: true,
+              },
+            },
+            tenant: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        }
+      );
+      return {
+        data: noSeriesLines,
+        message: "No Series Lines fetched successfully",
+      };
+    } catch (error: any) {
+      console.error("Error fetching No Series Lines:", error);
+      throw new Error("Failed to fetch No Series Lines");
+    }
+  };
+
+  getNoSeriesLine = async (id: string) => {
+    try {
+      const noSeriesLine = await this.findUnique(
+        (args) => this.db.noSeriesLine.findUnique(args),
+        {
+          where: { id },
+          include: {
+            noSeries: {
+              select: {
+                id: true,
+                code: true,
+                name: true,
+              },
+            },
+            company: {
+              select: {
+                id: true,
+                code: true,
+                name: true,
+              },
+            },
+            tenant: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        }
+      );
+
+      if (!noSeriesLine) {
+        throw new Error("No Series Line not found");
+      }
+
+      return {
+        data: noSeriesLine,
+        message: "No Series Line fetched successfully",
+      };
+    } catch (error: any) {
+      console.error("Error fetching No Series Line:", error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
         throw new Error(
-          "The ending date should be greater than the starting date."
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
         );
+      } else {
+        throw new Error(error.message);
       }
     }
+  };
 
-    const noSeriesLine = await db.noSeriesLine.update({
-      where: { id },
-      data: {
-        startingDate,
-        endingDate,
-        startingNo,
-        endingNo,
-        lastDateUsed,
-        lastNoUsed,
-        lastDigitUsed,
-        increment,
-      },
-    });
-    return {
-      data: noSeriesLine,
-      message: "No Series Line updated successfully",
-    };
-  } catch (error: any) {
-    console.error("Error updating No Series Line:", error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+  updateNoSeriesLine = async (
+    id: string,
+    noSeriesLine: Prisma.NoSeriesLineUncheckedCreateInput
+  ) => {
+    const {
+      startingDate,
+      endingDate,
+      startingNo,
+      endingNo,
+      lastDateUsed,
+      lastNoUsed,
+      lastDigitUsed,
+      increment,
+    } = noSeriesLine;
+    try {
+      //check if the noSeriesLine exists
+      const noSeriesLineExists = await this.findUnique(
+        (args) => this.db.noSeriesLine.findUnique(args),
+        {
+          where: { id },
+        }
       );
-    } else {
-      throw new Error(error.message);
-    }
-  }
-};
+      if (!noSeriesLineExists) {
+        throw new Error("No Series Line not found");
+      }
 
-const deleteNoSeriesLine = async (id: string) => {
-  try {
-    const noSeriesLine = await db.noSeriesLine.findUnique({
-      where: { id },
-    });
-    if (!noSeriesLine) {
-      throw new Error("No Series Line not found.");
-    }
+      if (startingDate && endingDate) {
+        if (startingDate > endingDate) {
+          throw new Error(
+            "The starting date should be less than the ending date."
+          );
+        }
+        if (endingDate < startingDate) {
+          throw new Error(
+            "The ending date should be greater than the starting date."
+          );
+        }
+      } else if (startingDate) {
+        if (noSeriesLineExists.endingDate) {
+          if (startingDate > noSeriesLineExists.endingDate) {
+            throw new Error(
+              "The starting date should be less than the ending date."
+            );
+          }
+          if (noSeriesLineExists.endingDate < startingDate) {
+            throw new Error(
+              "The ending date should be greater than the starting date."
+            );
+          }
+        }
+      } else if (endingDate && noSeriesLineExists.startingDate) {
+        if (endingDate < noSeriesLineExists.startingDate) {
+          throw new Error(
+            "The ending date should be greater than the starting date."
+          );
+        }
+      }
 
-    const noSeriesLineDeleted = await db.noSeriesLine.delete({
-      where: { id },
-    });
-    return {
-      data: noSeriesLineDeleted,
-      message: "No Series Line deleted successfully",
-    };
-  } catch (error: any) {
-    console.error("Error deleting No Series Line:", error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+      const startingNoUpper = startingNo
+        ? await slugify(startingNo)
+        : noSeriesLineExists.startingNo;
+
+      if (
+        (await slugify(startingNo)) &&
+        (await slugify(startingNo)) !== noSeriesLineExists.startingNo
+      ) {
+        const noSeriesLineByStartingNo = await this.findUnique(
+          (args) => this.db.noSeriesLine.findUnique(args),
+          { where: { startingNo } }
+        );
+        if (noSeriesLineByStartingNo) {
+          throw new Error(
+            `No Series Line with startingNo: ${startingNo} already exists`
+          );
+        }
+      }
+
+      const noSeriesLine = await this.update(
+        (args) => this.db.noSeriesLine.update(args),
+        {
+          where: { id },
+          data: {
+            startingDate,
+            endingDate,
+            startingNo: startingNoUpper,
+            endingNo,
+            lastDateUsed,
+            lastNoUsed,
+            lastDigitUsed,
+            increment,
+          },
+        }
       );
-    } else {
-      throw new Error(error.message);
+      return {
+        data: noSeriesLine,
+        message: "No Series Line updated successfully",
+      };
+    } catch (error: any) {
+      console.error("Error updating No Series Line:", error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
     }
-  }
-};
+  };
 
-export const noSeriesLineService = {
-  createNoSeriesLine,
-  getNoSeriesLines,
-  getNoSeriesLine,
-  updateNoSeriesLine,
-  deleteNoSeriesLine,
+  deleteNoSeriesLine = async (id: string) => {
+    try {
+      //check if the noSeriesLine exists
+      const noSeriesLine = await this.findUnique(
+        (args) => this.db.noSeriesLine.findUnique(args),
+        {
+          where: { id },
+        }
+      );
+      if (!noSeriesLine) {
+        throw new Error("No Series Line not found.");
+      }
+
+      const noSeriesLineDeleted = await this.delete(
+        (args) => this.db.noSeriesLine.delete(args),
+        {
+          where: { id },
+        }
+      );
+      return {
+        data: noSeriesLineDeleted,
+        message: "No Series Line deleted successfully",
+      };
+    } catch (error: any) {
+      console.error("Error deleting No Series Line:", error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
+    }
+  };
+}
+export const noSeriesLineService = (): NoSeriesLineService => {
+  return new NoSeriesLineService(db);
 };
