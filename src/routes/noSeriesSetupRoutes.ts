5bf1c627e63011971fd53876f3dd2682b86963f2
Ivan Epou <ivanepou92@gmail.com>
2024-10-25 17:10:10 +0300

    Add noSeriesSetupRoutes

---
+++ src/routes/noSeriesSetupRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  createNoSeriesSetup,
+  getNoSeriesSetups,
+  getNoSeriesSetup,
+  updateNoSeriesSetup,
+  deleteNoSeriesSetup,
+} from "@/controllers/noSeriesSetupController";
+
+const noSeriesSetupRouter = Router();
+
+noSeriesSetupRouter.post("/noSeriesSetup", createNoSeriesSetup);
+noSeriesSetupRouter.get("/noSeriesSetups", getNoSeriesSetups);
+noSeriesSetupRouter.get("/noSeriesSetup/:id", getNoSeriesSetup);
+noSeriesSetupRouter.put("/noSeriesSetup/:id", updateNoSeriesSetup);
+noSeriesSetupRouter.delete("/noSeriesSetup/:id", deleteNoSeriesSetup);
+
+export default noSeriesSetupRouter;
