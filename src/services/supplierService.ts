d846fc82e6814b5edda8846e806eb84696e03f24
Ivan Epou <ivanepou92@gmail.com>
2024-10-28 19:46:49 +0300

    Modify supplier indexes

--- src/services/supplierService.ts
+++ src/services/supplierService.ts
@@ -1,291 +1,356 @@
 import { db } from "@/db/db";
-import { Prisma } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { MultiTenantService } from "./multiTenantService";
+import { setNoSeries, updateNoSeries } from "@/utils/noSeriesManagement";
 
-const createSupplier = async (
-  supplier: Prisma.SupplierUncheckedCreateInput
-) => {
-  const {
-    supplierType,
-    name,
-    contactPerson,
-    phone,
-    email,
-    country,
-    location,
-    website,
-    taxPin,
-    regNumber,
-    bankAccountNumber,
-    bankName,
-    paymentTerms,
-    logo,
-    rating,
-    note,
-  } = supplier;
+class SupplierService extends MultiTenantService {
+  constructor(db: PrismaClient) {
+    super(db);
+  }
+  createSupplier = async (supplier: Prisma.SupplierUncheckedCreateInput) => {
+    const {
+      supplierType,
+      name,
+      phone,
+      email,
+      country,
+      location,
+      website,
+      taxPin,
+      regNumber,
+      bankAccountNumber,
+      bankName,
+      paymentTerms,
+      logo,
+      address,
+      address_2,
+      rating,
+      note,
+      vendorPostingGroupId,
+      genBusPostingGroupId,
+      vatBusPostingGroupId,
+      userId,
+    } = supplier;
 
-  try {
-    const supplierByPhone = await db.supplier.findUnique({
-      where: {
-        phone,
-      },
-    });
-    if (supplierByPhone) {
-      throw new Error(
-        `Phone Number: ${phone} is Already in use by another Supplier`
+    try {
+      const supplierNo = await setNoSeries(
+        this.getTenantId(),
+        this.getCompanyId(),
+        "vendorNos"
       );
-    }
-
-    if (email) {
-      const supplierByEmail = await db.supplier.findUnique({
-        where: {
-          email,
-        },
-      });
-      if (supplierByEmail) {
+      const supplierEx = await this.findUnique(
+        (args) => this.db.supplier.findUnique(args),
+        {
+          where: {
+            tenantId_companyId_No: {
+              No: supplierNo,
+              tenantId: this.getTenantId(),
+              companyId: this.getCompanyId(),
+            },
+          },
+        }
+      );
+      if (supplierEx) {
         throw new Error(
-          `Email: ${email} is Already in use by another Supplier`
+          `No: ${supplierNo} is Already in use by another Supplier`
         );
       }
-    }
 
-    if (regNumber) {
-      const supplierByRegNumber = await db.supplier.findUnique({
-        where: {
-          regNumber,
-        },
-      });
-      if (supplierByRegNumber) {
-        throw new Error(
-          `Registration Number: ${regNumber} is Already in use by another Supplier`
-        );
-      }
-    }
-
-    const newSupplier = await db.supplier.create({
-      data: {
-        supplierType,
-        name,
-        contactPerson,
-        phone,
-        email,
-        country,
-        location,
-        website,
-        taxPin,
-        regNumber,
-        bankAccountNumber,
-        bankName,
-        paymentTerms,
-        logo,
-        rating,
-        note,
-      },
-      include: {
-        products: true,
-        PurchaseHeaders: true,
-        SupplierLedgerEntries: true,
-        PurchaseReceiptHeaders: true,
-        PurchaseLines: true,
-        PurchaseReceiptLines: true,
-        vendorPostingGroup: true,
-        genBusPostingGroup: true,
-        vatBusPostingGroup: true,
-      },
-    });
-
-    return {
-      data: newSupplier,
-      message: "Supplier created Successfully.",
-    };
-  } catch (error: any) {
-    console.error(error);
-    throw new Error(error.message);
-  }
-};
-
-const getSuppliers = async () => {
-  try {
-    const suppliers = await db.supplier.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    return {
-      data: suppliers,
-      message: "Suppliers fetched successfully",
-    };
-  } catch (error: any) {
-    console.error(error);
-    throw new Error(error.message);
-  }
-};
-
-const getSupplier = async (id: string) => {
-  try {
-    const supplier = await db.supplier.findUnique({
-      where: {
-        id,
-      },
-    });
-    if (!supplier) {
-      throw new Error("Supplier not found");
-    }
-    return {
-      data: supplier,
-    };
-  } catch (error: any) {
-    console.error(error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+      const newSupplier = await this.create(
+        (args) => this.db.supplier.create(args),
+        {
+          data: {
+            supplierType,
+            name,
+            phone,
+            email,
+            country,
+            location,
+            website,
+            taxPin,
+            regNumber,
+            bankAccountNumber,
+            bankName,
+            paymentTerms,
+            logo,
+            address,
+            address_2,
+            rating,
+            note,
+            vendorPostingGroupId,
+            genBusPostingGroupId,
+            vatBusPostingGroupId,
+            userId,
+            companyId: this.getCompanyId,
+            tenantId: this.getTenantId,
+          },
+          include: {
+            products: true,
+            PurchaseHeaders: true,
+            SupplierLedgerEntries: true,
+            PurchaseReceiptHeaders: true,
+            PurchaseLines: true,
+            PurchaseReceiptLines: true,
+            vendorPostingGroup: true,
+            genBusPostingGroup: true,
+            vatBusPostingGroup: true,
+            company: {
+              select: {
+                code: true,
+                name: true,
+              },
+            },
+            tenant: {
+              select: {
+                name: true,
+              },
+            },
+            user: {
+              select: {
+                fullName: true,
+                email: true,
+              },
+            },
+          },
+        }
       );
-    } else {
+
+      await updateNoSeries(
+        this.getTenantId(),
+        this.getCompanyId(),
+        "vendorNos"
+      );
+
+      return {
+        data: newSupplier,
+        message: "Supplier created Successfully.",
+      };
+    } catch (error: any) {
+      console.error(error);
       throw new Error(error.message);
     }
-  }
-};
+  };
 
-const updateSupplier = async (
-  id: string,
-  supplier: Prisma.SupplierUncheckedCreateInput
-) => {
-  const {
-    supplierType,
-    name,
-    contactPerson,
-    phone,
-    email,
-    country,
-    location,
-    website,
-    taxPin,
-    regNumber,
-    bankAccountNumber,
-    bankName,
-    paymentTerms,
-    logo,
-    rating,
-    note,
-  } = supplier;
-  try {
-    const existingSupplier = await db.supplier.findUnique({
-      where: { id },
-    });
-
-    if (!existingSupplier) {
-      throw new Error("Supplier not found.");
-    }
-
-    if (phone && phone !== existingSupplier.phone) {
-      const supplierByPhone = await db.supplier.findUnique({
-        where: {
-          phone,
-        },
-      });
-      if (supplierByPhone) {
-        throw new Error(
-          `Phone Number: ${phone} is Already in use by another Supplier`
-        );
-      }
-    }
-
-    if (email && email !== existingSupplier.email) {
-      const supplierByEmail = await db.supplier.findUnique({
-        where: {
-          email,
-        },
-      });
-      if (supplierByEmail) {
-        throw new Error(
-          `Email: ${email} is Already in use by another Supplier`
-        );
-      }
-    }
-
-    if (regNumber && regNumber !== existingSupplier.regNumber) {
-      const supplierByRegNumber = await db.supplier.findUnique({
-        where: {
-          regNumber,
-        },
-      });
-      if (supplierByRegNumber) {
-        throw new Error(
-          `Registration Number: ${regNumber} is Already in use by another Supplier`
-        );
-      }
-    }
-    const updatedSupplier = await db.supplier.update({
-      where: { id },
-      data: {
-        supplierType: supplierType || existingSupplier.supplierType,
-        name: name || existingSupplier.name,
-        contactPerson: contactPerson || existingSupplier.contactPerson,
-        phone: phone || existingSupplier.phone,
-        email: email || existingSupplier.email,
-        country: country || existingSupplier.country,
-        location: location || existingSupplier.location,
-        website: website || existingSupplier.website,
-        taxPin: taxPin || existingSupplier.taxPin,
-        regNumber: regNumber || existingSupplier.regNumber,
-        bankAccountNumber:
-          bankAccountNumber || existingSupplier.bankAccountNumber,
-        bankName: bankName || existingSupplier.bankName,
-        paymentTerms: paymentTerms || existingSupplier.paymentTerms,
-        logo: logo || existingSupplier.logo,
-        rating: rating || existingSupplier.rating,
-        note: note || existingSupplier.note,
-      },
-    });
-
-    return {
-      data: updatedSupplier,
-      message: `Supplier updated successfully`,
-    };
-  } catch (error: any) {
-    console.error(error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+  getSuppliers = async () => {
+    try {
+      const suppliers = await this.findMany(
+        (args) => this.db.supplier.findMany(args),
+        {
+          orderBy: {
+            createdAt: "desc",
+          },
+          include: {
+            products: true,
+            PurchaseHeaders: true,
+            SupplierLedgerEntries: true,
+            PurchaseReceiptHeaders: true,
+            PurchaseLines: true,
+            PurchaseReceiptLines: true,
+            vendorPostingGroup: true,
+            genBusPostingGroup: true,
+            vatBusPostingGroup: true,
+            company: {
+              select: {
+                code: true,
+                name: true,
+              },
+            },
+            tenant: {
+              select: {
+                name: true,
+              },
+            },
+            user: {
+              select: {
+                fullName: true,
+                email: true,
+              },
+            },
+          },
+        }
       );
-    } else {
+      return {
+        data: suppliers,
+        message: "Suppliers fetched successfully",
+      };
+    } catch (error: any) {
+      console.error(error);
       throw new Error(error.message);
     }
-  }
-};
+  };
 
-const deleteSupplier = async (id: string) => {
-  try {
-    const existingSupplier = await db.supplier.findUnique({
-      where: { id },
-    });
-
-    if (!existingSupplier) {
-      throw new Error("Supplier not found");
-    }
-
-    await db.supplier.delete({
-      where: { id },
-    });
-
-    return {
-      message: `Supplier deleted successfully`,
-    };
-  } catch (error: any) {
-    console.error(error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+  getSupplier = async (id: string) => {
+    try {
+      const supplier = await this.findUnique(
+        (args) => this.db.supplier.findUnique(args),
+        {
+          where: {
+            id,
+          },
+          include: {
+            products: true,
+            PurchaseHeaders: true,
+            SupplierLedgerEntries: true,
+            PurchaseReceiptHeaders: true,
+            PurchaseLines: true,
+            PurchaseReceiptLines: true,
+            vendorPostingGroup: true,
+            genBusPostingGroup: true,
+            vatBusPostingGroup: true,
+            company: {
+              select: {
+                code: true,
+                name: true,
+              },
+            },
+            tenant: {
+              select: {
+                name: true,
+              },
+            },
+            user: {
+              select: {
+                fullName: true,
+                email: true,
+              },
+            },
+          },
+        }
       );
-    } else {
-      throw new Error(error.message);
+      if (!supplier) {
+        throw new Error("Supplier not found");
+      }
+      return {
+        data: supplier,
+      };
+    } catch (error: any) {
+      console.error(error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
     }
-  }
-};
+  };
 
-export const supplierService = {
-  createSupplier,
-  getSuppliers,
-  getSupplier,
-  updateSupplier,
-  deleteSupplier,
+  updateSupplier = async (
+    id: string,
+    supplier: Prisma.SupplierUncheckedCreateInput
+  ) => {
+    const {
+      supplierType,
+      name,
+      phone,
+      email,
+      country,
+      location,
+      website,
+      taxPin,
+      regNumber,
+      bankAccountNumber,
+      bankName,
+      paymentTerms,
+      logo,
+      address,
+      address_2,
+      rating,
+      note,
+      vendorPostingGroupId,
+      genBusPostingGroupId,
+      vatBusPostingGroupId,
+    } = supplier;
+    try {
+      const existingSupplier = await this.findUnique(
+        (args) => this.db.supplier.findUnique(args),
+        {
+          where: { id },
+        }
+      );
+      if (!existingSupplier) {
+        throw new Error("Supplier not found.");
+      }
+
+      const updatedSupplier = await this.update(
+        (args) => this.db.supplier.update(args),
+        {
+          where: { id },
+          data: {
+            supplierType,
+            name,
+            phone,
+            email,
+            country,
+            location,
+            website,
+            taxPin,
+            regNumber,
+            bankAccountNumber,
+            bankName,
+            paymentTerms,
+            logo,
+            address,
+            address_2,
+            rating,
+            note,
+            vendorPostingGroupId,
+            genBusPostingGroupId,
+            vatBusPostingGroupId,
+          },
+        }
+      );
+
+      return {
+        data: updatedSupplier,
+        message: `Supplier updated successfully`,
+      };
+    } catch (error: any) {
+      console.error(error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
+    }
+  };
+
+  deleteSupplier = async (id: string) => {
+    try {
+      const existingSupplier = await this.findUnique(
+        (args) => this.db.supplier.findUnique(args),
+        {
+          where: { id },
+        }
+      );
+
+      if (!existingSupplier) {
+        throw new Error("Supplier not found");
+      }
+
+      await this.delete((args) => this.db.supplier.delete(args), {
+        where: { id },
+      });
+
+      return {
+        message: `Supplier deleted successfully`,
+      };
+    } catch (error: any) {
+      console.error(error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
+    }
+  };
+}
+
+// Export the service
+export const supplierService = (): SupplierService => {
+  return new SupplierService(db);
 };
