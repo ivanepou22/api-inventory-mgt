f79c737c69563d7f13b8fbd6a953ab1942339d79
Ivan Epou <ivanepou92@gmail.com>
2024-11-03 17:22:00 +0300

    Add salesPersonRoutes

---
+++ src/routes/salesPersonRoutes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { salesPersonController } from "../controllers/salesPersonController";
+
+const salesPersonRouter = Router();
+
+salesPersonRouter.post(
+  "/salesPersons/",
+  salesPersonController.createSalesPerson
+);
+salesPersonRouter.get("/salesPersons/", salesPersonController.getSalesPersons);
+salesPersonRouter.get(
+  "/salesPersons/:id",
+  salesPersonController.getSalesPerson
+);
+salesPersonRouter.put(
+  "/salesPersons/:id",
+  salesPersonController.updateSalesPerson
+);
+salesPersonRouter.delete(
+  "/salesPersons/:id",
+  salesPersonController.deleteSalesPerson
+);
+
+export default salesPersonRouter;
