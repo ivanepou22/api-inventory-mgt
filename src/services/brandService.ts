3cb802b8f914bef37a633a8f01513c368eb14f72
Ivan Epou <ivanepou92@gmail.com>
2024-11-10 23:17:10 +0300

    Implement multitenancy in brandService

--- src/services/brandService.ts
+++ src/services/brandService.ts
@@ -1,164 +1,209 @@
 import { db } from "@/db/db";
-import { Prisma } from "@prisma/client";
-//create a service to create a brand
-export const createBrand = async (brand: Prisma.BrandCreateInput) => {
-  const { name, slug, image } = brand;
-  const brandExists = await db.brand.findUnique({
-    where: {
-      slug,
-    },
-  });
-  if (brandExists) {
-    throw new Error(`Brand with slug: ${slug} already exists`);
-  }
-  try {
-    const newBrand = await db.brand.create({
-      data: {
-        name,
-        slug,
-        image,
-      },
-    });
-    return {
-      data: newBrand,
-      error: null,
-      message: "Brand created successfully",
-    };
-  } catch (error: any) {
-    console.error("Error creating Brand:", error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(`An unexpected error occurred. Please try again later.`);
-    } else {
-      throw new Error(error.message);
-    }
-  }
-};
+import { Prisma, PrismaClient } from "@prisma/client";
+import { MultiTenantService } from "./multiTenantService";
+import { slugify } from "@/utils/functions";
 
-//Get all brands
-export const getBrands = async () => {
-  try {
-    const brands = await db.brand.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    return {
-      data: brands,
-    };
-  } catch (error: any) {
-    console.log(error.message);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(`An unexpected error occurred. Please try again later.`);
-    } else {
-      throw new Error(`An unexpected error occurred. Please try again later.`);
-    }
+class BrandService extends MultiTenantService {
+  constructor(db: PrismaClient) {
+    super(db);
   }
-};
 
-//Get a brand by id
-export const getBrand = async (id: string) => {
-  try {
-    const brand = await db.brand.findUnique({
-      where: {
-        id,
-      },
-    });
-    if (!brand) {
-      throw new Error("Brand not found.");
+  //create a service to create a brand
+  createBrand = async (brand: Prisma.BrandCreateInput) => {
+    const { name, code, image } = brand;
+    if (!name || !code) {
+      throw new Error("Name and code are required.");
     }
+    const codeSlug = await slugify(code);
 
-    return {
-      data: brand,
-    };
-  } catch (error: any) {
-    console.log(error);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
-      );
-    } else {
-      throw new Error(error.message);
-    }
-  }
-};
-
-//Update a brand
-export const updateBrand = async (
-  id: string,
-  brand: Prisma.BrandCreateInput
-) => {
-  const { name, slug, image } = brand;
-  try {
-    const brandExists = await db.brand.findUnique({
-      where: { id },
-      select: { id: true, name: true, slug: true, image: true },
-    });
-    if (!brandExists) {
-      throw new Error("Brand not found.");
-    }
-    if (slug && slug !== brandExists.slug) {
-      const brandBySlug = await db.brand.findUnique({
+    const brandExists = await this.findUnique(
+      (args) => this.db.brand.findUnique(args),
+      {
         where: {
-          slug,
+          tenantId_companyId_code: {
+            tenantId: this.tenantId,
+            companyId: this.companyId,
+            code: codeSlug,
+          },
+        },
+      }
+    );
+    if (brandExists) {
+      throw new Error(`Brand with slug: ${code} already exists`);
+    }
+    try {
+      const newBrand = await this.create((args) => this.db.brand.create(args), {
+        data: {
+          name,
+          code: codeSlug,
+          image,
         },
       });
-      if (brandBySlug) {
-        throw new Error(`Brand with slug: ${slug} already exists`);
+      return {
+        data: newBrand,
+        error: null,
+        message: "Brand created successfully",
+      };
+    } catch (error: any) {
+      console.error("Error creating Brand:", error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `An unexpected error occurred. Please try again later.`
+        );
+      } else {
+        throw new Error(error.message);
       }
     }
-    // Perform the update
-    const updatedBrand = await db.brand.update({
-      where: { id },
-      data: { name, slug, image },
-    });
-    return { data: updatedBrand, message: "Brand updated successfully" };
-  } catch (error: any) {
-    console.error("Error updating Brand:", error.message);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
-      );
-    } else {
-      throw new Error(error.message);
-    }
-  }
-};
+  };
 
-//create a service to delete a brand
-export const deleteBrand = async (id: string) => {
-  try {
-    // Check if the brand exists
-    const brand = await db.brand.findUnique({
-      where: { id },
-      select: { id: true },
-    });
-    if (!brand) {
-      throw new Error("Brand not found.");
-    }
-    // Delete the brand
-    const deletedBrand = await db.brand.delete({
-      where: { id },
-    });
-    return {
-      data: deletedBrand,
-      message: `Brand deleted successfully`,
-    };
-  } catch (error: any) {
-    console.error("Error deleting Brand:", error.message);
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      throw new Error(
-        `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+  //Get all brands
+  getBrands = async () => {
+    try {
+      const brands = await this.findMany(
+        (args) => this.db.brand.findMany(args),
+        {
+          orderBy: {
+            createdAt: "desc",
+          },
+        }
       );
-    } else {
-      throw new Error(error.message);
+      return {
+        data: brands,
+      };
+    } catch (error: any) {
+      console.log(error.message);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `An unexpected error occurred. Please try again later.`
+        );
+      } else {
+        throw new Error(
+          `An unexpected error occurred. Please try again later.`
+        );
+      }
     }
-  }
-};
+  };
 
-export const brandService = {
-  createBrand,
-  getBrands,
-  getBrand,
-  updateBrand,
-  deleteBrand,
+  //Get a brand by id
+  getBrand = async (id: string) => {
+    try {
+      const brand = await this.findUnique(
+        (args) => this.db.brand.findUnique(args),
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      if (!brand) {
+        throw new Error("Brand not found.");
+      }
+
+      return {
+        data: brand,
+      };
+    } catch (error: any) {
+      console.log(error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
+    }
+  };
+
+  //Update a brand
+  updateBrand = async (id: string, brand: Prisma.BrandCreateInput) => {
+    const { name, code, image } = brand;
+    try {
+      const brandExists = await this.findUnique(
+        (args) => this.db.brand.findUnique(args),
+        {
+          where: { id },
+          select: { id: true, code: true, name: true, image: true },
+        }
+      );
+      if (!brandExists) {
+        throw new Error("Brand not found.");
+      }
+
+      const codeSlug = code ? await slugify(code) : null;
+
+      if (codeSlug && codeSlug !== brandExists.code) {
+        const brandBySlug = await this.findUnique(
+          (args) => this.db.brand.findUnique(args),
+          {
+            where: {
+              tenantId_companyId_code: {
+                tenantId: brandExists.tenantId,
+                companyId: brandExists.companyId,
+                code: codeSlug,
+              },
+            },
+          }
+        );
+        if (brandBySlug) {
+          throw new Error(`Brand with slug: ${codeSlug} already exists`);
+        }
+      }
+      // Perform the update
+      const updatedBrand = await this.update(
+        (args) => this.db.brand.update(args),
+        { where: { id }, data: { name, code: codeSlug, image } }
+      );
+      return { data: updatedBrand, message: "Brand updated successfully" };
+    } catch (error: any) {
+      console.error("Error updating Brand:", error.message);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
+    }
+  };
+
+  //create a service to delete a brand
+  deleteBrand = async (id: string) => {
+    try {
+      // Check if the brand exists
+      const brand = await this.findUnique(
+        (args) => this.db.brand.findUnique(args),
+        {
+          where: { id },
+          select: { id: true },
+        }
+      );
+      if (!brand) {
+        throw new Error("Brand not found.");
+      }
+      // Delete the brand
+      const deletedBrand = await this.delete(
+        (args) => this.db.brand.delete(args),
+        {
+          where: { id },
+        }
+      );
+      return {
+        data: deletedBrand,
+        message: `Brand deleted successfully`,
+      };
+    } catch (error: any) {
+      console.error("Error deleting Brand:", error.message);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new Error(
+          `The provided ID "${id}" is invalid. It must be a 12-byte hexadecimal string, but it is 25 characters long.`
+        );
+      } else {
+        throw new Error(error.message);
+      }
+    }
+  };
+}
+
+export const brandService = (): BrandService => {
+  return new BrandService(db);
 };
